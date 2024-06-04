e303f187a276684c71d512c1039a3aa84c6af7a6
Anes <anes.mulalic@outlook.com>
2024-06-04 23:01:09 +0200

    feat(social-links)

--- src/components/dialog/Popup.tsx
+++ src/components/dialog/Popup.tsx
@@ -1,3 +1,4 @@
+import CloseIcon from "@components/ui/icons/close";
 import {
   Button,
   Dialog,
@@ -14,9 +15,8 @@ export default function Popup({ title, text, isOpen, close }) {
         <Dialog
           as="div"
           className="relative z-50 focus:outline-none"
-          onClose={close}
-          __demoMode>
-          <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+          onClose={close}>
+          <div className="fixed inset-0 bg-black/70" aria-hidden="true" />
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4">
               <TransitionChild
@@ -26,18 +26,38 @@ export default function Popup({ title, text, isOpen, close }) {
                 leave="ease-in duration-200"
                 leaveFrom="opacity-100 transform-[scale(100%)]"
                 leaveTo="opacity-0 transform-[scale(95%)]">
-                <DialogPanel className="w-full text-center max-w-md rounded-xl bg-background p-6 backdrop-blur-2xl">
+                <DialogPanel className="relative w-full text-center max-w-md rounded-xl bg-background p-6 backdrop-blur-2xl">
                   <DialogTitle
                     as="h3"
-                    className="text-base font-semibold text-white">
+                    className="text-lg font-semibold text-white">
                     {title}
                   </DialogTitle>
-                  <p className="mt-2 text-sm/6 text-gray-light">{text}</p>
+                  <p className="mt-2 text-sm text-gray-light">{text}</p>
+                  <div className="mt-4 flex flex-row items-center justify-center gap-4 w-full">
+                    <a
+                      className="flex items-center justify-center w-10 h-10 bg-background-dark hover:bg-background-dark/70 rounded-full"
+                      href="https://www.instagram.com/japodium/"
+                      target="_blank"
+                      rel="noopener">
+                      <img
+                        src="ig.svg"
+                        alt="Facebook"
+                        className="w-5 h-5 fill-primary"
+                      />
+                    </a>
+                    <a
+                      className="flex items-center justify-center w-10 h-10 bg-background-dark hover:bg-background-dark/70 rounded-full"
+                      href="https://www.facebook.com/japodium/"
+                      target="_blank"
+                      rel="noopener">
+                      <img src="fb.svg" alt="Twitter" className="w-5 h-5" />
+                    </a>
+                  </div>
                   <div className="mt-4">
                     <Button
-                      className="inline-flex items-center gap-2 rounded-md bg-background-dark py-1.5 px-3 text-sm/6 font-semibold text-primary focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      className="absolute top-2 right-2 inline-flex items-center gap-2 rounded-full p-2 opacity-60 hover:opacity-100 outline-none focus:outline-none"
                       onClick={close}>
-                      Got it, thanks!
+                      <CloseIcon className={"w-3 h-3"} />
                     </Button>
                   </div>
                 </DialogPanel>
