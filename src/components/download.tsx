d63d67ba605cffae3ad9676ae21a9e4c601bcecd
Anes <anes.mulalic@outlook.com>
2024-08-29 16:27:52 +0200

    feat(links): update app store link

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { useState } from "react";
-import Popup from "./dialog/Popup.js";
+// import { useState } from "react";
+// import Popup from "./dialog/Popup.js";
 import Appstore from "./ui/icons/appstore.jsx";
 import Play from "./ui/icons/play.jsx";
 
 export default function Download() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [isDisabled, setIsDisabled] = useState(false);
 
-  function open() {
-    setIsOpen(true);
-    setIsDisabled(true);
-  }
+  // function open() {
+  //   setIsOpen(true);
+  //   setIsDisabled(true);
+  // }
 
-  function close() {
-    setIsOpen(false);
-  }
+  // function close() {
+  //   setIsOpen(false);
+  // }
   return (
     <>
       <div className="flex-1 flex flex-col gap-8 justify-center max-w-xs static z-40">
@@ -27,19 +27,21 @@ export default function Download() {
           <Play className={"max-h-[36px]"} />
         </a>
         <a
-          onClick={open}
-          href="#"
+          href="https://apps.apple.com/in/app/japodium/id6615062017"
+          target="_blank"
+          // onClick={open}
+          // href="#"
           className={`bg-primary rounded-lg py-3 flex items-center justify-center border-0 hover:opacity-90 active:opacity-90`}
           rel="noopener">
           <Appstore className={"max-h-[36px]"} />
         </a>
       </div>
-      <Popup
+      {/* <Popup
         isOpen={isOpen}
         close={close}
         title="Stay tuned!"
         text="Our app will soon be available on the App Store. Thank you for your interest. Follow us for the latest updates!"
-      />
+      /> */}
     </>
   );
 }
