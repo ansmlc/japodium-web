7fa3c4d4dc076b803b5700afcd95d6bc107e787f
ansmlc <anes.mulalic@outlook.com>
2026-08-16 16:09:52 +0200

    feat: update android button - coming soon

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -1,31 +1,28 @@
-import React from "react";
-// import { useState } from "react";
-// import Popup from "./dialog/Popup.js";
+import React, { useState } from "react";
+import Popup from "./dialog/Popup";
 import Appstore from "./ui/icons/appstore.jsx";
 import Play from "./ui/icons/play.jsx";
 
 export default function Download() {
-  // const [isOpen, setIsOpen] = useState(false);
-  // const [isDisabled, setIsDisabled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // function open() {
-  //   setIsOpen(true);
-  //   setIsDisabled(true);
-  // }
+  function open() {
+    setIsOpen(true);
+  }
 
-  // function close() {
-  //   setIsOpen(false);
-  // }
+  function close() {
+    setIsOpen(false);
+  }
   return (
     <>
       <div className="flex-1 flex flex-col gap-8 justify-center max-w-xs static z-40">
-        <a
-          href="https://play.google.com/store/apps/details?id=japodium.android"
-          target="_blank"
+        <button
+          type="button"
+          onClick={open}
           className="bg-primary rounded-lg py-3 flex items-center justify-center border-0 hover:opacity-90 active:opacity-90"
-          rel="noopener">
+          aria-label="Android app coming soon">
           <Play className={"max-h-[36px]"} />
-        </a>
+        </button>
         <a
           href="https://apps.apple.com/in/app/japodium/id6615062017"
           target="_blank"
@@ -52,12 +49,12 @@ export default function Download() {
           <img src="fb.svg" alt="Twitter" className="w-5 h-5" />
         </a>
       </div>
-      {/* <Popup
+      <Popup
         isOpen={isOpen}
         close={close}
-        title="Stay tuned!"
-        text="Our app will soon be available on the App Store. Thank you for your interest. Follow us for the latest updates!"
-      /> */}
+        title="Coming soon"
+        text="Our Android app will be available soon."
+      />
     </>
   );
 }
