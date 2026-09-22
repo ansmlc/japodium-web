2584ff217ddf695049cfa936951ed968807aff94
Anes <anes.mulalic@outlook.com>
2026-09-22 17:45:59 +0200

    feat(download): update android url

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -1,28 +1,19 @@
-import React, { useState } from "react";
-import Popup from "./dialog/Popup";
+import React from "react";
 import Appstore from "./ui/icons/appstore.jsx";
 import Play from "./ui/icons/play.jsx";
 
 export default function Download() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function open() {
-    setIsOpen(true);
-  }
-
-  function close() {
-    setIsOpen(false);
-  }
   return (
     <>
       <div className="flex-1 flex flex-col gap-8 justify-center max-w-xs static z-40">
-        <button
-          type="button"
-          onClick={open}
+        <a
+          href="https://play.google.com/store/apps/details?id=japodium.android"
+          target="_blank"
           className="bg-primary rounded-lg py-3 flex items-center justify-center border-0 hover:opacity-90 active:opacity-90"
-          aria-label="Android app coming soon">
+          rel="noopener"
+          aria-label="Download Japodium on Google Play">
           <Play className={"max-h-[36px]"} />
-        </button>
+        </a>
         <a
           href="https://apps.apple.com/in/app/japodium/id6615062017"
           target="_blank"
@@ -49,12 +40,6 @@ export default function Download() {
           <img src="fb.svg" alt="Twitter" className="w-5 h-5" />
         </a>
       </div>
-      <Popup
-        isOpen={isOpen}
-        close={close}
-        title="Coming soon"
-        text="Our Android app will be available soon."
-      />
     </>
   );
 }
